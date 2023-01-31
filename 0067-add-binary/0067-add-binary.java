5470c7325e213c674c3f8be43db2e88e47996a6d
Ayesha Irshad <104616632+AyeshaIrshad1337@users.noreply.github.com>
2023-01-31 23:27:30 +0500

    Time: 6 ms (26.13%), Space: 42.5 MB (49.95%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,8 +1,10 @@
-import java.math.*;
+    import java.math.BigInteger;
 class Solution {
-    public String addBinary(String a, String b) {
-        BigInteger n=new BigInteger(a,2);
-        BigInteger m=n.add(new BigInteger(b,2));
-        return m.toString(2);
-    }
+
+public String addBinary(String a, String b) {
+        return new BigInteger(a, 2)
+            .add(new BigInteger(b, 2))
+            .toString(2);
+}
+
 }
